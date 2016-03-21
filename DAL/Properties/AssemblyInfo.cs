fd2de7bac453fd1ad38d5c9651d0dc4715d91a56
ststeiger <quandary82@hailmail.net>
2016-03-21 09:46:19 +0100

    BuildOptions changed

--- DAL/Properties/AssemblyInfo.cs
+++ DAL/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.3.16.1701")]
-[assembly: AssemblyFileVersion("2016.3.16.1701")]
+[assembly: AssemblyVersion("2016.3.21.0942")]
+[assembly: AssemblyFileVersion("2016.3.21.0942")]
